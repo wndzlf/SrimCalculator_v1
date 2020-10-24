1486ef00df6f85f06f11d17056bad1bed75c930f
최원석 <cws653@naver.com>
2020-10-24 14:21:54 +0900

    SecondViewController & ThirdViewController 커밋, 푸쉬

---
+++ SrimCalculator/SrimCalculator/DataStruct.swift
@@ -0,0 +1,14 @@
+//
+//  DataStruct.swift
+//  SrimCalculator
+//
+//  Created by 최원석 on 2020/10/24.
+//  Copyright © 2020 최원석. All rights reserved.
+//
+
+import Foundation
+
+
+struct CorpCodeJson {
+    <#fields#>
+}
